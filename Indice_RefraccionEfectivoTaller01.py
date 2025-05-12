22ac5b98e8e783662176b9866fde88feeca490f3
labravo20 <labravo@unal.edu.co>
2025-05-12 16:10:56 -0500

    Insertando funciones para cálculo de constantes de normalización

--- Indice_RefraccionEfectivoTaller01.py
+++ Indice_RefraccionEfectivoTaller01.py
@@ -18,6 +18,30 @@ def indice_RefraccionEfectivoTrazadoRayos(angulo_Tetah, indice_RefraccionNucleo)
 # Índice de refracción del núcleo del guía de onda
 n_nucleo = 1.5
 
+# Índice de refracción del cladding del guía de onda
+n_cladding = 1
+
+#Longitud de onda 
+longitud_onda = 1E-6 #Unidades en METROS [m]
+
+#Ancho del núcleo del guía de onda
+ancho_Nucleo = 1E-6 #Unidades en METROS [m]
+
+#Velocidad de la luz
+c = 3E8 #Unidades en METROS/SEGUNDO [m/s]
+
+#Número de onda
+numero_OndaVacio = 2*np.pi/longitud_onda
+
+#Frecuencia angular
+frecuencia_Angular = numero_OndaVacio*c
+
+#Permeabilidad en el vacío
+permeabilidad_vacio = (4E-7)*np.pi #Unidades H/m
+
+
+""" Definiendo paŕametros de tetah"""
+
 # Ángulo tetah asociado al modo de estudio --> TALLER PARTE 01
 tetah_modo0TE = 1.3095852
 tetah_modo1TE = 1.0383224
@@ -45,6 +69,15 @@ print(resultado_indiceRefraccionEfectivomodo1TM)
 print(resultado_indiceRefraccionEfectivomodo2TM)
 
 
+""" Calculando constantes de propagación Beta para cada uno de los modos """
+beta_modo0TE = resultado_indiceRefraccionEfectivomodo0TE*numero_OndaVacio
+beta_modo1TE = resultado_indiceRefraccionEfectivomodo1TE*numero_OndaVacio
+beta_modo2TE = resultado_indiceRefraccionEfectivomodo2TE*numero_OndaVacio
+beta_modo0TM = resultado_indiceRefraccionEfectivomodo0TM*numero_OndaVacio
+beta_modo1TM = resultado_indiceRefraccionEfectivomodo1TM*numero_OndaVacio
+beta_modo2TM = resultado_indiceRefraccionEfectivomodo2TM*numero_OndaVacio
+
+
 """ Definiendo función para el cálculo del índice de refracción efectivo --> MÉTODO ANÁLISIS ONDULATORIO """
 """ CONSIDERACIÓN --> En desarrollo método gráfico x = (kappa um)/2; y = (gamma um)/2"""
 
@@ -67,14 +100,7 @@ def indice_RefraccionEfectivoOndulatorio(indice_RefraccionNucleo, indice_Refracc
     return indice_refraccionEfectivo
 
 
-""" Definición de variables asociadas al problema de estudio """
-
-# Índice de refracción del núcleo del guía de onda
-n_nucleo = 1.5
-
-# Índice de refracción del cladding del guía de onda
-n_cladding = 1
-
+""" Definición parámetros calculados para cada uno de los modos --> En caso de análisis ondulatorio """
 
 # Coordenadas de interceptos --> TALLER PARTE 02
 modo0TE = [1.2169799538,3.294839197]
@@ -106,7 +132,7 @@ print(resultado_indiceRefraccionEfectivomodo2TM)
 
 # Se define variable en la cual se asignará el cálculo del índice de refracción efectivo
 
-""" MODIFICACIÓN DE CASO """
+"""  MODIFICACIÓN DE CASO """
 caso = 1
 
 resultado_indiceRefraccionEfectivomodo0TE = indice_RefraccionEfectivoOndulatorio(n_nucleo,n_cladding,modo0TE[0],modo0TE[1])
@@ -126,3 +152,47 @@ print(resultado_indiceRefraccionEfectivomodo2TE)
 print(resultado_indiceRefraccionEfectivomodo0TM)
 print(resultado_indiceRefraccionEfectivomodo1TM)
 print(resultado_indiceRefraccionEfectivomodo2TM)
+
+
+"""  Definiendo función para cálculo de contantes de NORMALIZACIÓN, en función de cada modo """
+
+def constantes_Normalizacion(frecuencia_Angular,constante_propagacionBeta,ancho_nucleo,coordenadaY,coordenadaX,permeabilidad_Vacio):
+
+    #Calculando constante c1 de normalización
+    C1 = np.sqrt((4*frecuencia_Angular*permeabilidad_Vacio)/(constante_propagacionBeta*(ancho_nucleo + (1/coordenadaY))))
+
+    #Calculando constante c0 de normalización
+    C0 = C1 * np.cos(coordenadaX*ancho_nucleo)
+
+    "Definiendo variable a la cual se le asignan las contantes de normalización calculadas "
+    constantes_Normalizacion = [C0,C1]
+
+
+    return constantes_Normalizacion
+
+
+
+# Se define variable en la cual se asignará el cálculo de las constantes de propagación
+
+constantes_NormalizacionModo0TE = constantes_Normalizacion(frecuencia_Angular,beta_modo0TE,ancho_Nucleo,modo0TE[1],modo0TE[0],
+                                                           permeabilidad_vacio)
+constantes_NormalizacionModo1TE = constantes_Normalizacion(frecuencia_Angular,beta_modo1TE,ancho_Nucleo,modo1TE[1],modo1TE[0],
+                                                           permeabilidad_vacio)
+constantes_NormalizacionModo2TE = constantes_Normalizacion(frecuencia_Angular,beta_modo2TE,ancho_Nucleo,modo2TE[1],modo2TE[0],
+                                                           permeabilidad_vacio)
+constantes_NormalizacionModo0TM = constantes_Normalizacion(frecuencia_Angular,beta_modo0TM,ancho_Nucleo,modo0TM[1],modo0TM[0],
+                                                           permeabilidad_vacio)
+constantes_NormalizacionModo1TM = constantes_Normalizacion(frecuencia_Angular,beta_modo1TM,ancho_Nucleo,modo1TM[1],modo1TM[0],
+                                                           permeabilidad_vacio)
+constantes_NormalizacionModo2TM = constantes_Normalizacion(frecuencia_Angular,beta_modo2TM,ancho_Nucleo,modo2TM[1],modo2TM[0],
+                                                           permeabilidad_vacio)
+
+
+# Mostrando resultados en consola
+print(constantes_NormalizacionModo0TE)
+print(constantes_NormalizacionModo1TE)
+print(constantes_NormalizacionModo2TE)
+print(constantes_NormalizacionModo0TM)
+print(constantes_NormalizacionModo1TM)
+print(constantes_NormalizacionModo2TM)
+
